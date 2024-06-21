230ea1fee50b60eab575427fd474ce6da7d75460
BadreddineZatout <hb_zatout@esi.dz>
2024-06-21 14:52:58 +0100

    feat: app layout

---
+++ insight-front/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="text-white bg-gray-600 flex flex-wrap justify-around items-center py-20 font-bold"
+  >
+    <div class="text-end">
+      <h1 class="text-2xl font-bold mb-10">الصفحةالثانية</h1>
+      <ul class="space-y-3">
+        <li>اﻟﺴﺠﻼت اﻟﺘﺠﺎرﻳﺔ في اﻟﻤﻤﻠﻜﺔ</li>
+        <li>تطور أعداد السجلات التجارية</li>
+        <li>إحصائيات إضافية</li>
+        <li>المهن الاستشارية</li>
+        <li>حماية المستهلك</li>
+      </ul>
+    </div>
+    <div class="text-end">
+      <h1 class="text-2xl font-bold mb-10">الصفحة الأولى</h1>
+      <ul class="space-y-3">
+        <li>اﻟﺴﺠﻼت اﻟﺘﺠﺎرﻳﺔ في اﻟﻤﻤﻠﻜﺔ</li>
+        <li>تطور أعداد السجلات التجارية</li>
+        <li>إحصائيات إضافية</li>
+        <li>المهن الاستشارية</li>
+        <li>حماية المستهلك</li>
+      </ul>
+    </div>
+    <div class="text-end flex items-center gap-5">
+      <div>
+        <h1 class="text-4xl">وزارة التجــارة</h1>
+        <h2 class="text-lg">Ministry of Commerce</h2>
+      </div>
+      <img width="100" src="/logo.png" alt="logo" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ insight-front/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pt-10 w-full text-white bg-black">
+    <div
+      class="w-full flex flex-wrap justify-center gap-20 items-center text-xl font-semibold px-10"
+    >
+      <div class="text-end flex items-center gap-5">
+        <div>
+          <h1 class="text-2xl">وزارة التجــارة</h1>
+          <h2 class="text-xs">Ministry of Commerce</h2>
+        </div>
+        <img width="100" src="/logo.png" alt="logo" />
+      </div>
+      <RouterLink class="text-center w-fit flex-shrink" to="#">
+        حماية <br />المستهلك
+      </RouterLink>
+      <RouterLink class="text-center w-fit flex-shrink" to="#">
+        المهن <br />الاستشارية
+      </RouterLink>
+      <RouterLink class="text-center w-fit flex-shrink" to="#">
+        إحصائيات <br />إضافية
+      </RouterLink>
+      <RouterLink class="text-center w-fit flex-shrink" to="#">
+        تطور أعداد <br />السجلات التجارية
+      </RouterLink>
+      <RouterLink class="text-center w-fit flex-shrink" to="#">
+        اﻟﺴﺠﻼت اﻟﺘﺠﺎرﻳﺔ <br />في اﻟﻤﻤﻠﻜﺔ
+      </RouterLink>
+    </div>
+    <div class="flex justify-center items-center gap-10 py-20 font-bold">
+      <h1 class="text-7xl text-end">إحصائيات <br />في المملكة</h1>
+      <h1 class="text-[12rem] text-blue-500">2024</h1>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
